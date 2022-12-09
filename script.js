d3ff35a78dbbf6978ba3f735e6467c4d0cae52df
Ushmani <ushmani.garg@gmail.com>
2022-12-09 12:18:31 +0530

    Javascript add

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message){
+const formControl = input.parentElement;
+formControl.className = 'form-control error';
+}
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(username.value===''){
+        showError('Username is required');
+    }else{
+        showSuccess(username);
+    }
+});
